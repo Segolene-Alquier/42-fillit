40d28e276ef06844e12cfde572dc387264e7552a
bafraiki <bafraiki@student.42.fr>
2018-12-18 13:24:35 +0100

    check finished need to casse la gueule

--- src/check.c
+++ src/check.c
@@ -6,13 +6,13 @@
 /*   By: bafraiki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/17 15:16:36 by bafraiki          #+#    #+#             */
-/*   Updated: 2018/12/17 18:11:06 by bafraiki         ###   ########.fr       */
+/*   Updated: 2018/12/18 13:23:29 by bafraiki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-int		ft_strlen_strchr(char *str)
+int		ft_strlen_strchr(char *str, int *count)
 {
 	int i;
 
@@ -21,9 +21,11 @@ int		ft_strlen_strchr(char *str)
 	{
 		if (str[i] != '#' && str[i] != '.')
 			exit(EXIT_FAILURE);
+		if (str[i] == '#')
+			(*count)++;
 		i++;
 	}
-	return (i);
+		return (i);
 }
 
 void	ft_store_grid(int fd ,char **grid)
@@ -49,18 +51,19 @@ int	ft_grid_validity(int fd)
 	int nb_hash;
 
 	i = 0;
+	nb_hash = 0;
 	ft_store_grid(fd, grid);
 	while (grid[i])
 	{
 		nb_line = 0;
 		while (grid[i] && *grid[i] != '\0' && nb_line < 4)
 		{
-			if (ft_strlen_strchr(grid[i]) != 4)
+			if (ft_strlen_strchr(grid[i], &nb_hash) != 4)
 				exit (EXIT_FAILURE);
 			nb_line++;
 			i++;
 		}
-		if (nb_line != 4 || (grid[i] && ((!grid[i + 1]) || (*grid[i] != '\0'))))
+		if (nb_hash != 4 || nb_line != 4 || (grid[i] && ((!grid[i + 1]) || (*grid[i] != '\0'))))
 			exit(EXIT_FAILURE);
 		i++;
 	}
