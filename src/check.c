5e9951294e46de066200fa0de2cb1ddcb04fa4d3
bafraiki <bafraiki@student.42.fr>
2018-12-19 14:43:02 +0100

    gros git add .

--- src/check.c
+++ src/check.c
@@ -6,7 +6,7 @@
 /*   By: bafraiki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/17 15:16:36 by bafraiki          #+#    #+#             */
-/*   Updated: 2018/12/19 13:20:38 by bafraiki         ###   ########.fr       */
+/*   Updated: 2018/12/19 14:22:25 by bafraiki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,11 +57,9 @@ int	ft_grid_validity(int fd)
 	ft_store_grid(fd, grid);
 	while (grid[i] && (nb_line = 0) == 0)
 	{
-		while (grid[i] && *grid[i] != '\0' && nb_line < 4)
-			if (ft_strlen_strchr(grid[i], &nb_hash) != 4)
+		while (grid[i] && *grid[i] != '\0' && nb_line++ < 4)
+			if (ft_strlen_strchr(grid[i++], &nb_hash) != 4)
 				exit (EXIT_FAILURE);
-			else if (++nb_line > 0)  //one line to earn
-				i++;
 		if (nb_hash != 4 || nb_line != 4
 				|| (grid[i] && ((!grid[i + 1]) || (*grid[i] != '\0'))))
 			exit(EXIT_FAILURE);
