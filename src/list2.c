7fed8c808ade4078fb01eede5e992171d80a8499
bafraiki <bafraiki@student.42.fr>
2018-12-21 16:07:58 +0100

    list2 are good

--- src/list2.c
+++ src/list2.c
@@ -6,9 +6,11 @@
 /*   By: bafraiki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/21 15:10:31 by bafraiki          #+#    #+#             */
-/*   Updated: 2018/12/21 15:21:39 by bafraiki         ###   ########.fr       */
+/*   Updated: 2018/12/21 16:07:32 by bafraiki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "fillit.h"
 
 t_letter	*ft_new_letter(char c)
 {
@@ -23,22 +25,19 @@ t_letter	*ft_new_letter(char c)
 void	add_new_letter(t_letter **begin, char c)
 {
 	t_letter *elem;
-	t_letter tmp;
+	t_letter *tmp;
 
 	if (!*begin)
 	{
 		*begin = ft_new_letter(c);
 		return ;
 	}
-	else
-	{
-		elem = *begin;
-		while (elem->next && elem->letter < c)
-			if (elem->letter == c)   //not especially necessary but for dev we must be prudent
-				return ;
-			else
-				elem = elem->next;
-	}
+	elem = *begin;
+	while (elem->next && elem->letter <= c)
+		if (elem->letter == c || elem->next->letter == c) //not especially necessary but for dev we must be prudent
+			return ;
+		else
+			elem = elem->next;
 	if (elem->next)
 	{
 		tmp = elem->next;
@@ -49,25 +48,29 @@ void	add_new_letter(t_letter **begin, char c)
 		elem->next = ft_new_letter(c);
 }
 
-void	remove_letter(t_letter *begin, char c)
+void	remove_letter(t_letter **begin, char c)
 {
 	t_letter *elem;
 	t_letter *tmp;
 
-	elem = begin;
+	elem = *begin;
+	if (elem->letter == c && elem->next && (*begin = elem->next) != NULL)
+	{
+		free(elem);
+		return ;
+	}
 	while (elem->next && elem->next->letter != c)
 		elem = elem->next;
-	if (elem->next && elem->next->letter == c)
+	if (elem->next && elem->next->letter == c && (tmp = elem->next) != NULL)
 	{
-		tmp = elem->next;
 		elem->next = elem->next->next;
 		free(tmp);
 	}
-	else if (elem->letter == c && (begin = NULL) == NULL)
+	else if (elem->letter == c && (*begin = NULL) == NULL)
 		free(elem);
 	else if (elem->next->letter == c)
 	{
 		free(elem->next);
-		elem->next == NULL;
+		elem->next = NULL;
 	}
 }
